d73af485785508375ac577f52d8378bad26b7ad6
Pontus Widén <pontare.widen@gmail.com>
2018-06-11 15:15:08 +0200

    Fixade chart för alla kommuner

--- Presentation/Controllers/MunicipalityController.cs
+++ Presentation/Controllers/MunicipalityController.cs
@@ -109,5 +109,6 @@ namespace Presentation.Controllers
             Session["perInhabitant"] = id;
             return RedirectToAction((string)Session["chosenAction"]);
         }
+       
     }
 }
